745eca18696c74527efbbfec7aa5d14c307b67ab
VietHoang1512 <phanviethoang1512@gmail.com>
2022-04-11 14:38:03 +0000

    Add data

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir data
 cd data
-# gdown --id 1GMCuS77aJ4PVtTSlaAMP6zuSf4LWjzv3
-# gdown --id 1ptG5W568wB-9EXVKDs2avZLCUc2ozW0o
-# gdown --id 1d6eYH2WOL4oZ1RrKZIQDwPxEDLxCk1eT
+gdown --id 1GMCuS77aJ4PVtTSlaAMP6zuSf4LWjzv3
+gdown --id 1ptG5W568wB-9EXVKDs2avZLCUc2ozW0o
+gdown --id 1d6eYH2WOL4oZ1RrKZIQDwPxEDLxCk1eT
 unzip '*.zip'
 cd ..

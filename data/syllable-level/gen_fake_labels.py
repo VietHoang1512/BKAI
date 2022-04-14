5b1c2729b26d4fb3dcd39559d163e21cbd966e67
VietHoang1512 <phanviethoang1512@gmail.com>
2022-04-14 09:09:16 +0000

    Add scripts

--- data/syllable-level/gen_fake_labels.py
+++ data/syllable-level/gen_fake_labels.py
@@ -1,13 +1,11 @@
 import os
-from socket import inet_ntoa
-
 
 
 if __name__ == "__main__":
     datadir = "test"
-    random_intent = "greeting"
+    random_intent = "smart.home.set.percentage"
     random_slot = "O"
-    
+
     with open(os.path.join(datadir, "seq.in"), "r") as f:
         sequences = []
         for line in f:
@@ -15,9 +13,11 @@ if __name__ == "__main__":
     with open(os.path.join(datadir, "label"), "w") as f:
         f.write("\n".join(["{}".format(random_intent) for _ in sequences]))
     with open(os.path.join(datadir, "seq.out"), "w") as f:
-        f.write("\n".join([" ".join(["{}".format(random_slot) for word in sequence.split()]) for sequence in sequences]))
+        f.write("\n".join([" ".join(["{}".format(random_slot)
+                for word in sequence.split()]) for sequence in sequences]))
     with open(os.path.join(datadir, "results.csv"), "w") as f:
         for i in range(len(sequences)):
-            line ="{}, ".format(random_intent) 
-            line = line + " ".join(["{}".format(random_slot) for word in sequences[i].split()])
-            f.write(line+"\n")
+            line = "{}, ".format(random_intent)
+            line = line + " ".join(["{}".format(random_slot)
+                                   for word in sequences[i].split()])
+            f.write(line.strip()+"\n")
--- run.sh
+++ run.sh
@@ -1,30 +1,33 @@
 #As we initialize JointIDSF from JointBERT, user need to train a base model JointBERT first
 
 #Train JointIDSF
-export lr=4e-5
-export c=0.15
+export lr=2e-5
 export s=100
 echo "${lr}"
 export MODEL_DIR=outputs
-export MODEL_DIR=$MODEL_DIR"/"$lr"/"$c"/"$s
-echo "${MODEL_DIR}"
-python3 main.py --token_level syllable-level \
-                  --model_type phobert \
-                  --model_dir $MODEL_DIR \
-                  --data_dir data \
-                  --seed $s \
-                  --do_train \
-                  --do_eval \
-                  --save_steps 140 \
-                  --logging_steps 140 \
-                  --num_train_epochs 50 \
-                  --tuning_metric mean_intent_slot \
-                  --use_intent_context_attention \
-                  --attention_embedding_size 200 \
-                  --use_crf \
-                  --gpu_id 0 \
-                  --embedding_type soft \
-                  --intent_loss_coef $c \
-                  --pretrained \
-                  --pretrained_path vinai/phobert-base \
-                  --learning_rate $lr
+
+for c in .1 .3 .5 .7 .9
+do
+    export MODEL_DIR=$MODEL_DIR"/"$lr"/"$c"/"$s
+    echo "${MODEL_DIR}"
+    python3 main.py --token_level syllable-level \
+                    --model_type phobert \
+                    --model_dir $MODEL_DIR \
+                    --data_dir data \
+                    --seed $s \
+                    --do_train \
+                    --do_eval \
+                    --save_steps 140 \
+                    --logging_steps 140 \
+                    --num_train_epochs 100 \
+                    --tuning_metric mean_intent_slot \
+                    --use_intent_context_attention \
+                    --attention_embedding_size 200 \
+                    --use_crf \
+                    --gpu_id 0 \
+                    --embedding_type soft \
+                    --intent_loss_coef $c \
+                    --pretrained \
+                    --pretrained_path vinai/phobert-base \
+                    --learning_rate $lr
+done
